758b7fd80f6ed692d5c6dae17b328056cbb18fcc
ArtemZhuravlev <Utah954@gmail.com>
2024-02-06 20:32:00 +0200

    Initial commit

---
+++ src/conf/config.py
@@ -0,0 +1,5 @@
+class Config:
+    DB_URL = "postgresql+asyncpg://postgres:15061992@localhost:5432/WEB_11"
+
+
+config = Config
---
+++ src/schemas/todo.py
@@ -0,0 +1,32 @@
+from datetime import date
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+
+class ContactSchema(BaseModel):  # TodoSchema
+    firstname: str = Field("default", min_length=3, max_length=15)
+    lastname: str = Field(min_length=3, max_length=15)
+    #email: EmailStr = Field()
+    email: str = EmailStr
+    #phone: int = Field(description="The contact phone number")
+    phone: str = Field(min_length=3, max_length=25)
+    birthday: Optional[date] = Field(None, description="The birthday date Day-Month-Year")
+    completed: Optional[bool] = False
+
+
+class ContactUpdateSchema(ContactSchema):
+    completed: bool
+
+
+class ContactResponse(BaseModel):
+    id: int = 1
+    firstname: str
+    lastname: str
+    email: str
+    phone: str
+    birthday: Optional[date]
+    completed: bool
+
+    class Config:
+        from_attributes: True
